bc03a5652d76fbb626b75166030f939952c6ff23
KaiFu <powerkaifu@gmail.com>
2025-06-27 04:45:06 +0800

    Render 測試

--- traffic_main/settings.py
+++ traffic_main/settings.py
@@ -16,20 +16,15 @@ import environ
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 隱藏非錯誤日誌
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-fj-$0fwg%2ak(234kgijo#(8ojvae&263zj%iqp5mf+tbuq%k4"
 SECRET_KEY = env("DJANGO_SECRET_KEY", default = "unsafe-default")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -86,8 +81,6 @@ WSGI_APPLICATION = "traffic_main.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
