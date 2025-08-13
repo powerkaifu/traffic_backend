9cf97d410a295a14e1db514a2dce1134011615c1
KaiFu <powerkaifu@gmail.com>
2025-08-13 15:45:46 +0800

    訓練模型

--- traffic_main/settings.py
+++ traffic_main/settings.py
@@ -80,16 +80,20 @@ WSGI_APPLICATION = "traffic_main.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default = 'localhost'),
-        'PORT': env('DB_PORT', default = '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        # MySQL 設定（已註解）
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': env('DB_NAME'),
+        # 'USER': env('DB_USER'),
+        # 'PASSWORD': env('DB_PASSWORD'),
+        # 'HOST': env('DB_HOST', default = 'localhost'),
+        # 'PORT': env('DB_PORT', default = '3306'),
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #     'charset': 'utf8mb4',
+        # },
+        # 改用 SQLite 設定
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
